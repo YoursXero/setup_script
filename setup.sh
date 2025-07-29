74bf11f18a6fe42059a2315c9d52c8d10b6e2211
GHOST | ゴースト <90827302+Ghosuto@users.noreply.github.com>
2025-07-29 10:56:28 +0530

    Update script
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- setup.sh
+++ setup.sh
@@ -47,12 +47,9 @@ declare -A REPOS=(
     ["device/nothing/Pong"]="https://github.com/Pong-Development/device_nothing_Pong.git"
     ["vendor/nothing/Pong"]="https://github.com/Pong-Development/vendor_nothing_Pong.git"
     ["kernel/nothing/sm8475"]="https://github.com/Pong-Development/kernel_nothing_sm8475.git"
+    ["kernel/nothing/sm8475-modules"]="https://github.com/Nothing-phone-2-Development/android_kernel_nothing_sm8475-modules.git"
+    ["kernel/nothing/sm8475-devicetrees"]="https://github.com/Nothing-phone-2-Development/android_kernel_nothing_sm8475-devicetrees.git"
     ["hardware/qcom-caf/sm8450/display"]="https://github.com/Pong-Development/hardware_qcom-caf_sm8450_display.git"
-    ["hardware/qcom-caf/sm8450/audio/primary-hal"]="https://github.com/Pong-Development/hardware_qcom_audio-ar.git"
-    ["hardware/qcom-caf/sm8450/audio/pal"]="https://github.com/Pong-Development/vendor_qcom_opensource_arpal-lx.git"
-    ["hardware/qcom-caf/sm8450/audio/agm"]="https://github.com/Pong-Development/vendor_qcom_opensource_agm.git"
-    ["hardware/qcom-caf/sm8450/audio/graphservices"]="https://github.com/Pong-Development/vendor_qcom_opensource_audioreach-graphservices.git"
-    ["device/qcom/sepolicy_vndr/sm8450"]="https://github.com/Pong-Development/device_qcom_sepolicy_vndr.git"
     ["packages/apps/ParanoidGlyphPhone2"]="https://github.com/Pong-Development/packages_apps_ParanoidGlyph.git"
     ["packages/apps/GlyphAdapter"]="https://github.com/Pong-Development/packages_apps_GlyphAdapter.git"
     ["hardware/dolby"]="https://github.com/Pong-Development/hardware_dolby.git"
@@ -109,7 +106,7 @@ done
 # KernelSU patch
 echo "Applying KernelSU patch..."
 cd kernel/nothing/sm8475 || exit 1
-git submodule init && git submodule update
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v2.0.0
 cd - >/dev/null
 echo ""
 echo ""
